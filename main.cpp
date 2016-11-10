6082d3fa41935b99e08ce206dcdf27ab15366b3c
Vladimir Rujević <vladimir.rujevic@student.um.si>
2016-11-10 13:32:57 +0100

    Debug - zombie
    Odpravljanje napake pri zapiranju začetnega okna

--- main.cpp
+++ main.cpp
@@ -11,12 +11,14 @@ int main(int argc, char *argv[])
   QDialog* d = new QDialog(0,0);
   Ui_ZacetnoOkno zUi;
   zUi.setupUi(d);
-  int s = d->exec();
+  int s = d->exec(), st = 0;
   if(s == QDialog::Rejected){
     w.close();
+    a.exit(0);
   } else if (s == QDialog::Accepted){
     w.show();
     w.start();
+    st = a.exec();
   }
-  return a.exec();
+  return st;
 }
